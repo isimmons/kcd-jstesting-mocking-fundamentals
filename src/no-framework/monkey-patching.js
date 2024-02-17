679ef284d62ce93a9f04ac44397b9e645d45c372
isimmons <isimmons33@gmail.com>
2024-02-17 04:43:50 -0500

    monkeys

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,29 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+// monkey patching the utils.getWinner function
+const originalGetWinner = utils.getWinner;
+// override to be a function that always returns
+// player 1 so we don't have random pass/fail tests
+utils.getWinner = (p1, p2) => p1;
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+// run the test for thumbWar which now uses
+// the monkey patched version of getWinner
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 
 // cleanup
-utils.getWinner = originalGetWinner
+// set getWinner back to it's original state
+utils.getWinner = originalGetWinner;
+
+// Think about what is going on above
+// we are mocking getWinner and giving it a
+// different implementation that we can control
+// for our test.
+
+// then like running an afterEach function
+// we perform a mock reset by setting getWinner
+// back to it's original state.
+
+// read more about monkeys... and monkey patching
